93086577c3b34f84e03686fbdd01c705f3c08cfb
Mingfa Ma <mingfa.ma@deepnetsecurity.com>
2024-12-02 16:11:37 +0000

    chart worked

--- symmetric-webconsole/src/main/frontend/views/throughput/ThroughputView.tsx
+++ symmetric-webconsole/src/main/frontend/views/throughput/ThroughputView.tsx
@@ -1,9 +1,40 @@
+import HillaStat from 'Frontend/generated/com/jumpmind/symmetric/console/ui/data/HillaStat';
+import { ProAPIEndpoint } from 'Frontend/generated/endpoints';
+import { useEffect, useState } from 'react';
+import { ResponsiveContainer, AreaChart, CartesianGrid, XAxis, YAxis, Tooltip, Area } from 'recharts';
+
 export default function ThroughputView() {
+
+  const [chartData, setChartData] = useState<(HillaStat | undefined)[]>([]);
+  useEffect(() => {
+
+    ProAPIEndpoint.convert2Chart().then(cd => setChartData(cd || []));
+  }, []);  
+
   return (
-    <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img style={{ width: '300px' }} src="images/deepnet-logo.png" />
-      <h2>Deepnet Security MySQL Replication v1.0</h2>
-      <p>Powered by SymmetricDS 🤗</p>
-    </div>
+    <ResponsiveContainer width="100%" height="100%">
+      <AreaChart
+        width={500}
+        height={400}
+        data={chartData}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="strDate" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="routed" stackId="1" stroke="#89729E" fill="#89729E" />
+        <Area type="monotone" dataKey="extracted" stackId="1" stroke="#89C4F4" fill="#89C4F4" />
+        <Area type="monotone" dataKey="sent" stackId="1" stroke="#87D37C" fill="#87D37C" />
+        <Area type="monotone" dataKey="loaded" stackId="1" stroke="#FFD700" fill="#FFD700" />
+        <Area type="monotone" dataKey="unrouted" stackId="1" stroke="#f39c12" fill="#f39c12" />                
+      </AreaChart>
+    </ResponsiveContainer>
   );
 }
+
