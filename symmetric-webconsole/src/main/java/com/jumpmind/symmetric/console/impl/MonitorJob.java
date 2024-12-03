1745f687491ee0b5a174950fb11e8531b179f0b7
Mingfa Ma <mingfa.ma@deepnetsecurity.com>
2024-12-03 10:15:56 +0000

    add monitor job

---
+++ symmetric-webconsole/src/main/java/com/jumpmind/symmetric/console/impl/MonitorJob.java
@@ -0,0 +1,36 @@
+package com.jumpmind.symmetric.console.impl;
+
+import com.jumpmind.symmetric.console.service.IMonitorService;
+import org.jumpmind.symmetric.ISymmetricEngine;
+import org.jumpmind.symmetric.job.AbstractJob;
+import org.jumpmind.symmetric.job.JobDefaults;
+import org.jumpmind.util.RandomTimeSlot;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+public class MonitorJob extends AbstractJob {
+   private static final String a = "MonitorJob";
+
+   public MonitorJob(ISymmetricEngine engine) {
+      this.engine = engine;
+      this.jobName = "Monitor";
+      this.parameterService = engine.getParameterService();
+      this.randomTimeSlot = new RandomTimeSlot(this.parameterService.getExternalId(), this.parameterService.getInt("job.random.max.start.time.ms"));
+      this.log = LoggerFactory.getLogger(a);
+   }
+
+   public MonitorJob(ISymmetricEngine engine, ThreadPoolTaskScheduler taskScheduler) {
+      super("Monitor", engine, taskScheduler);
+      this.log = LoggerFactory.getLogger(a);
+   }
+
+   public JobDefaults getDefaults() {
+      return new JobDefaults().schedule("10000").description("Run monitors and generate notifications");
+   }
+
+   public void doJob(boolean force) throws Exception {
+      if (this.engine != null) {
+         ((IMonitorService)this.engine.getExtensionService().getExtensionPoint(IMonitorService.class)).update();
+      }
+   }
+}
